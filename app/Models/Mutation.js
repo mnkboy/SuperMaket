7b71280d91990a51e1ba1b90e2587830e3ce8e52
mnkboy <thefouriusmonkeyboy@gmail.com>
2019-07-09 16:24:44 -0500

    Se agrega el ultimo Mutation actualizado

--- app/Models/Mutation.js
+++ app/Models/Mutation.js
@@ -1,89 +1,296 @@
-const AuthorModel = require('../models/Author');
-const authenticate = require('../utils/authenticate');
-const PostModel = require('../models/Post');
-const storage = require('../utils/storege');
+const PropuestaModel = require('../models/propuesta');
+const VentaModel = require('../models/venta');
+const HistorialModel = require('../models/historial');
+const ClienteModel = require('../models/cliente');
+const RepartidorModel = require('../models/Repartidor');
+const PermisoModel = require('../models/Permiso');
+const RolModel = require('../models/Rol');
+const CuentaModel = require('../models/Cuenta');
+const PedidoModel = require('../models/Pedido');
+const VendedorModel = require('../models/Vendedor');
 
-const createAuthor = async(root, params, context, info) => {
+/* #region Propuesta*/
+const crearPropuesta = async(root, params, context, info) => {
 
-	const newAuthor = await AuthorModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema');});
+	const newPropuesta = await PropuestaModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear la propuestas');});
 
-	if (!newAuthor) throw new Error('No se creo el author');
+	if (!newPropuesta) throw new Error('No se creo la propuesta');
 
-	return newAuthor;
+	return newPropuesta;
 };
 
-const login = async(root, params, context,info) =>{
-
-	const token = await authenticate(params).catch( e => { throw e;});
-
-	return {
-		token,
-		message: 'OK'
-	};
-};
-
-const updateProfile = async(root, params, context, info) =>{
-	const {data} = params;
-	const {user} = context;
-	let Author = await AuthorModel.findById(user._id);
-	if(!Author) throw new Error('El author no existe');
-	Author = {...Author,...data};
-	const updatedAuthor = await Author.save({new :true});
-	return updatedAuthor;
-};
-
-const deleteProfile = async(root, params, context, info) =>{
-	const {user} = context;
-	const Author = await AuthorModel.findById({_id:user._id});
-	if(!Author) throw new Error('El author no existe');
-	Author.is_active = false;
-	await Author.save({new:true});
-
-	return 'Usuario eliminado';
-};
-
-const createPost = async(root, params, context, info) =>{
-	const {user} = context;
-	params.data.author = user;
-	if(params.data.cover_photo){
-		const {createReadStream }= await params.data.cover_photo;
-		const stream = createReadStream();
-		const {url} = await storage({stream});
-
-		params.data.cover_photo = url;
-	}
-
-	const post = await PostModel.create(params.data)
-		.catch(e => {throw new Error('Error al crear el post');});
-	const newPost = PostModel.findById(post._id).populate('author');
-	console.log(post._author);
-	await AuthorModel.findOneAndUpdate({_id:user._id},{$push:{posts:post._id}});
-
-
-	return newPost;
-};
-
-const updatePost = async(root, params, context, info) =>{
+const actualizarPropuesta = async(root, params, context, info) =>{
 	const {id, data} = params;
 	const {user} = context;
-	const updatePost = await PostModel.findOneAndUpdate({_id:id,author:user._id},{$set:{...data}}, {new:true});
-	return updatePost.toObject();
+	const actualizarPropuesta = await PropuestaModel.findOneAndUpdate({_id:id},{$set:{...data}}, {new:true});
+	return actualizarPropuesta.toObject();
 };
 
-const deletePost = async(root, params, context, info)=>{
+const eliminarPropuesta = async(root, params, context, info)=>{
 	const {id, data} = params;
 	const {user} = context;
-	await PostModel.findOneAndUpdate({_id:id,author:user},{$set:{is_active:false}});
+	await PropuestaModel.findOneAndUpdate({_id:id},{$set:{is_active:false}});
    
-	return 'Post Elimindado';
+	return 'Propuesta Elimindada';
 };
 
-module.exports ={
-	createAuthor,
-	login,
-	updateProfile,
-	deleteProfile,
-	createPost,
-	updatePost,
-	deletePost
+
+/*region Venta */
+
+const crearVenta = async(root, params, context, info) => {
+
+	const newVenta = await VentaModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear la Venta');});
+
+	if (!newVenta) throw new Error('No se creo la Venta');
+
+	return newVenta;
 };
+
+const actualizarVenta = async(root, params, context, info) =>{
+	const {id, data} = params;
+	const {user} = context;
+	const actualizarVenta = await VentaModel.findOneAndUpdate({_id:id._id},{$set:{...data}}, {new:true});
+	return actualizarVenta.toObject();
+};
+
+const eliminarVenta = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {user} = context;
+	await VentaModel.findOneAndUpdate({_id:id},{$set:{is_active:false}});
+   
+	return 'Venta Elimindada';
+};
+
+
+/*#region Historial */
+
+const crearHistorial = async(root, params, context, info) => {
+
+	const newHistorial = await HistorialModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear el Historials');});
+
+	if (!newHistorial) throw new Error('No se creo el Historial');
+
+	return newHistorial;
+};
+
+const eliminarHistorial = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {user} = context;
+	await HistorialModel.findOneAndUpdate({_id:id},{$set:{is_active:false}});
+   
+	return 'Historial Elimindado';
+};
+
+
+/*#region Cliente */
+const crearCliente = async(root, params, context, info) => {
+
+	const newCliente = await ClienteModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear el Clientes');});
+
+	if (!newCliente) throw new Error('No se creo el Cliente');
+
+	return newCliente;
+};
+
+const actualizarCliente = async(root, params, context, info) =>{
+	const {id, data} = params;
+	const {user} = context;
+	const actualizarCliente = await ClienteModel.findOneAndUpdate({_id:id._id},{$set:{...data}}, {new:true});
+	return actualizarCliente.toObject();
+};
+
+const eliminarCliente = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {user} = context;
+	await ClienteModel.findOneAndUpdate({_id:id},{$set:{is_active:false}});
+   
+	return 'Cliente Elimindado';
+};
+
+/*#region Repartidor */
+const crearRepartidor = async(root, params, context, info) => {
+
+	const newRepartidor = await RepartidorModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear el Repartidor');});
+
+	if (!newRepartidor) throw new Error('No se creo el Repartidor');
+
+	return newRepartidor;
+};
+
+const actualizarRepartidor = async(root, params, context, info) =>{
+	const {id, data} = params;
+	const {user} = context;
+	const actualizarRepartidor = await RepartidorModel.findOneAndUpdate({_id:id._id},{$set:{...data}}, {new:true});
+	return actualizarRepartidor.toObject();
+};
+
+const eliminarRepartidor = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {user} = context;
+	await RepartidorModel.findOneAndUpdate({_id:id},{$set:{is_active:false}});
+   
+	return 'Repartidor Elimindado';
+};
+
+/*region Permiso */
+const crearPermiso = async(root, params, context, info) => {
+
+	const newPermiso = await PermisoModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear el Permiso');});
+
+	if (!newPermiso) throw new Error('No se creo el Permiso');
+
+	return newPermiso;
+};
+
+const actualizarPermiso = async(root, params, context, info) =>{
+	const {id, data} = params;
+	const {user} = context;
+	const actualizarPermiso = await PermisoModel.findOneAndUpdate({_id:id,permiso:user._id},{$set:{...data}}, {new:true});
+	return actualizarPermiso.toObject();
+};
+
+const eliminarPermiso = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {user} = context;
+	await PermisoModel.findOneAndUpdate({_id:id,permiso:user},{$set:{activo:false}});
+   
+	return 'Repartidor Elimindado';
+};
+
+/*region Cuenta */
+const crearCuenta = async(root, params, context, info) => {
+
+	const newCuenta = await CuentaModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear el Permiso');});
+
+	if (!newCuenta) throw new Error('No se creo el Permiso');
+
+	return newCuenta;
+};
+
+const actualizarCuenta = async(root, params, context, info) =>{
+	const {id, data} = params;
+	const {user} = context;
+	const actualizarCuenta = await CuentaModel.findOneAndUpdate({_id:id,cuenta:user._id},{$set:{...data}}, {new:true});
+	return actualizarCuenta.toObject();
+};
+
+const eliminarCuenta = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {user} = context;
+	await CuentaModel.findOneAndUpdate({_id:id,cuenta:user},{$set:{activo:false}});
+   
+	return 'Repartidor Elimindado';
+};
+
+/*region Rol */
+const crearRol = async(root, params, context, info) => {
+
+	const newCuenta = await RolModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear el Permiso');});
+
+	if (!newCuenta) throw new Error('No se creo el Permiso');
+
+	return newRol;
+};
+
+const actualizarRol = async(root, params, context, info) =>{
+	const {id, data} = params;
+	const {user} = context;
+	const actualizarRol = await RolModel.findOneAndUpdate({_id:id,rol:user._id},{$set:{...data}}, {new:true});
+	return updateRepartid.toObject();
+};
+
+const eliminarRol = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {user} = context;
+	await RolModel.findOneAndUpdate({_id:id,rol:user},{$set:{activo:false}});
+   
+	return 'Repartidor Elimindado';
+};
+
+/*region Pedido */
+const crearPedido = async(root, params, context, info) => {
+
+	const newCuenta = await PedidoModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear el Permiso');});
+
+	if (!newCuenta) throw new Error('No se creo el Permiso');
+
+	return newPedido;
+};
+
+const actualizarPedido = async(root, params, context, info) =>{
+	const {id, data} = params;
+	const {user} = context;
+	const actualizarPedido = await PedidoModel.findOneAndUpdate({_id:id,rol:user._id},{$set:{...data}}, {new:true});
+	return actualizarPedido.toObject();
+};
+
+const eliminarPedido = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {pedido} = context;
+	await PedidoModel.findOneAndUpdate({_id:id,rol:user},{$set:{activo:false}});
+   
+	return 'Repartidor Elimindado';
+};
+
+/*region Vendedor */
+const crearVendedor = async(root, params, context, info) => {
+
+	const newCuenta = await VendedorModel.create(params.data).catch( e => {throw new Error('Ocurrio un problema al crear el Permiso');});
+
+	if (!newCuenta) throw new Error('No se creo el Permiso');
+
+	return newPedido;
+};
+
+const actualizarVendedor = async(root, params, context, info) =>{
+	const {id, data} = params;
+	const {user} = context;
+	const actualizarVendedor = await VendedorModel.findOneAndUpdate({_id:id,rol:user._id},{$set:{...data}}, {new:true});
+	return actualizarVendedor.toObject();
+};
+
+const eliminarVendedor = async(root, params, context, info)=>{
+	const {id, data} = params;
+	const {pedido} = context;
+	await VendedorModel.findOneAndUpdate({_id:id,rol:user},{$set:{activo:false}});
+   
+	return 'Repartidor Elimindado';
+};
+
+
+module.exports = {
+    crearPropuesta,
+    actualizarPropuesta,
+    eliminarPropuesta,
+    crearVenta,
+    actualizarVenta,
+    eliminarVenta,
+    crearHistorial,
+    eliminarHistorial,
+    crearCliente,
+    actualizarCliente,
+    eliminarCliente,
+    crearRepartidor,
+    actualizarRepartidor,
+	eliminarRepartidor,
+	
+	crearCuenta,
+	actualizarCuenta,
+	eliminarCuenta,
+
+	crearPedido,
+	actualizarPedido,
+	eliminarPedido,
+
+	crearPermiso,
+	actualizarPermiso,
+	eliminarPermiso,
+
+	crearVendedor,
+	actualizarVendedor,
+	eliminarVendedor,
+
+	crearRol,
+	actualizarRol,
+	eliminarRol
+}
